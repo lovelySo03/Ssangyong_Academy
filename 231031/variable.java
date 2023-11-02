5cb09055a418d0a92d7bbbfce813f75482b8916a
lovelySo03 <143456833+lovelySo03@users.noreply.github.com>
2023-11-02 15:03:12 +0900

    Create variable.java

---
+++ 231031/variable.java
@@ -0,0 +1,59 @@
+public class MySample1031 {
+	
+	public static void main(String[] args) {
+		
+		int year = 0;
+		int age = 15;
+		
+		System.out.println("year: " + year);
+		System.out.println("age: " + age);
+		
+		year = age + 2000;
+		age = age + 1;
+		
+		System.out.println(year);		
+		System.out.println(age);				
+		
+	}
+	
+}
+
+/*
+public class MySample1031 {
+	
+	public static void main(String[] args) {
+		
+		int x, y, z;
+		int a=1, b=2, c=3;
+		
+		x = 10;
+		y = 20;
+		z = 30;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+}
+*/
+
+/*
+x와 y의 자리 바꾸기 
+public class MySample1031 {
+	
+	public static void main(String[] args) {
+		
+		int x = 10, y = 20;
+		int temp = 0;
+		
+		System.out.println("x:" + x + " y:" + y);
+		
+		temp = x;
+		x = y;
+	    y = temp;
+		
+		
+		System.out.println("x:" + x + " y:" + y);
+	}
+}
+*/
