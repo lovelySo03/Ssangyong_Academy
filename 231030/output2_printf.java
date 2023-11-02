245b4f756bd897534a4c09b2ee5abca5e736f22f
lovelySo03 <143456833+lovelySo03@users.noreply.github.com>
2023-11-02 14:49:05 +0900

    Create output2_printf.java

---
+++ 231030/output2_printf.java
@@ -0,0 +1,12 @@
+public class Hello3 {
+	
+	public static void main(String[] args) {
+		
+		// printf는 엔터를 포함하고 있지 않다. 
+		System.out.printf("수식을 출력하면 계산 결과 출력됩니다.\n %d + %d = %d입니다.", 10, 5, 10+5);
+ 		System.out.printf("\n내 생일은 %d년 %d월 %d 입니다.", 2016-11, 5, 8);
+ 		
+
+	}
+	
+}
