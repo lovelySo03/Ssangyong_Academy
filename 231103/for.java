842b916b07cff8ccbfb22eb8d846063567524995
lovelySo03 <143456833+lovelySo03@users.noreply.github.com>
2023-11-06 17:35:45 +0900

    Create for.java

---
+++ 231103/for.java
@@ -0,0 +1,12 @@
+public class MySample1103 {
+
+	public static void main(String[] args) {
+	
+		// 공백은 탭 't' 사용 
+		int i = 0;
+		int j = 10;
+		
+		for(i=0, j=10; i<10; i++, j--) {
+			System.out.printf("%d \t %d \n",i+1,j);		}
+	}
+}
